8ad3fced4ca1e564ee59522479ac408394347544
azizys <36770253+azizys@users.noreply.github.com>
2019-01-14 16:01:45 -0500

    Player
    Added basic capsule model, material, and basic movement script to get us going

---
+++ Cooper Game/Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public float hspeed;
+    public float zspeed;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // TODO: Move this logic into a charMove/charController script
+        float dx = Input.GetAxis("Horizontal");
+        float dz = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(dx * hspeed, 0.0f, dz * zspeed);
+        rb.velocity = movement;
+    }
+}
